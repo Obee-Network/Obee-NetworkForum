589a78cd915ef08534fceb4c7338a5b8e78c87d6
Link-Cruise <279988706@qq.com>
2020-05-07 18:13:39 +0800

    update

--- api/phone/get_multi_users.php
+++ api/phone/get_multi_users.php
@@ -11,6 +11,30 @@
 $json_error_data   = array();
 $json_success_data = array();
 $type              = Wo_Secure($_GET['type'], 0);
+if ($continue == false) {
+    $json_error_data = array(
+        'api_status' => '400',
+        'api_text' => 'failed',
+        'api_version' => $api_version,
+        'errors' => array(
+            'error_id' => '6',
+            'error_text' => 'One of the users is not exists.'
+        )
+    );
+    header("Content-type: application/json");
+    echo json_encode($json_error_data, JSON_PRETTY_PRINT);
+    exit();
+} else {
+    $json_success_data = array(
+        'api_status' => '200',
+        'api_text' => 'success',
+        'api_version' => $api_version,
+        'users' => $json_success_data
+    );
+    header("Content-type: application/json");
+    echo json_encode($json_success_data);
+    exit();
+}
 if ($type == 'get_multi_users') {
     if (empty($_POST['user_id'])) {
         $json_error_data = array(
@@ -100,30 +124,6 @@ if ($type == 'get_multi_users') {
         echo json_encode($json_error_data, JSON_PRETTY_PRINT);
         exit();
     }
-}
-if ($continue == false) {
-    $json_error_data = array(
-        'api_status' => '400',
-        'api_text' => 'failed',
-        'api_version' => $api_version,
-        'errors' => array(
-            'error_id' => '6',
-            'error_text' => 'One of the users is not exists.'
-        )
-    );
-    header("Content-type: application/json");
-    echo json_encode($json_error_data, JSON_PRETTY_PRINT);
-    exit();
-} else {
-    $json_success_data = array(
-        'api_status' => '200',
-        'api_text' => 'success',
-        'api_version' => $api_version,
-        'users' => $json_success_data
-    );
-    header("Content-type: application/json");
-    echo json_encode($json_success_data);
-    exit();
 }
 header("Content-type: application/json");
 echo json_encode($json_success_data);
